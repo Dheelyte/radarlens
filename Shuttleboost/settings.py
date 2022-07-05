00a8156d5504cd26c5e9a26c8f2f151adbcd6c3b
Dheelyte <olagbujidelight@gmail.com>
2022-07-05 14:06:38 +0100

    Settings.py: Remove whitenoise from MIDDLEWARE

--- Shuttleboost/settings.py
+++ Shuttleboost/settings.py
@@ -57,7 +57,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     #'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
