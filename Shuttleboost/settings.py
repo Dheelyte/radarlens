52d3e5fe709fdb68a599d94643f15e4403206e42
Dheelyte <olagbujidelight@gmail.com>
2022-07-06 19:27:47 +0100

    settings.py: Replace STATIC_ROOT with STATICFILES_DIR

--- Shuttleboost/settings.py
+++ Shuttleboost/settings.py
@@ -183,7 +183,7 @@ AWS_STORAGE_BUCKET_NAME = 'linque-media'
 
 AWS_S3_FILE_OVERWRITE = False
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = BASE_DIR / 'static'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_ROOT = BASE_DIR / 'media'
