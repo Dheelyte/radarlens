c8839c404882426769aa7c0e39e35641d055d572
Dheelyte <olagbujidelight@gmail.com>
2022-07-06 20:26:25 +0100

    settings.py: STATICFILES_DIRS: Make as a list (Add brackets)

--- Shuttleboost/settings.py
+++ Shuttleboost/settings.py
@@ -186,7 +186,7 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_LOCATION = 'static'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
-STATICFILES_DIRS = BASE_DIR / 'static'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_ROOT = BASE_DIR / 'media'
