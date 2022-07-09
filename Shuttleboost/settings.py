95ae7d59e37c76310bcd0814ffec5e58cfb04313
Dheelyte <olagbujidelight@gmail.com>
2022-07-09 12:35:26 +0100

    settings.py: Add AWS_LOCATION='media'

--- Shuttleboost/settings.py
+++ Shuttleboost/settings.py
@@ -182,6 +182,7 @@ AWS_SECRET_ACCESS_KEY = '1b0NP/UeefGpwSYEcbIN3xPY9Xb4pcfQpBhepFDR'
 AWS_STORAGE_BUCKET_NAME = 'linque-media'
 
 AWS_S3_FILE_OVERWRITE = False
+AWS_LOCATION = 'media'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
