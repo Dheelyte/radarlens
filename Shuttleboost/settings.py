a7aa0e85362acfad64793fcd74be02fc6261577f
Dheelyte <olagbujidelight@gmail.com>
2022-07-09 03:15:48 +0100

    settings.py: uninstall django-csp and remove from settings.MIDDLEWARE. Make style modifications

--- Shuttleboost/settings.py
+++ Shuttleboost/settings.py
@@ -61,7 +61,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'csp.middleware.CSPMiddleware',
 ]
 
 ROOT_URLCONF = 'Shuttleboost.urls'
