e6c28c6af569d2deee607b18c07444ac8e38cc2c
Dheelyte <olagbujidelight@gmail.com>
2022-07-09 17:04:55 +0100

    Correct storage backend before push. Add storage_backends.py and change DEFAULT_FILE_STORAGE to custome storage backend

--- Shuttleboost/storage_backends.py
+++ Shuttleboost/storage_backends.py
@@ -1,5 +1,5 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
-    bucket_name = 'my-app-bucket'
+    bucket_name = 'linque-media'
     location = 'media'
